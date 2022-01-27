4aa05158dbe64b190934e32efccfa4fd471dbca7
vikas hiwase <vikas.hiwase20@gmail.com>
2022-01-27 15:38:25 +0530

    widgets added

---
+++ src/components/widgetLg/WidgetLg.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./widgetLg.css";
+
+function WidgetLg() {
+  const Button = ({ type }) => {
+    return <button className={"widgetLgButton " + type}>{type}</button>;
+  };
+
+  return (
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="person"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">12 Jan 2022</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="person"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">12 Jan 2022</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Declined" />
+          </td>
+        </tr>
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="person"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">12 Jan 2022</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+
+        <tr className="widgetLgTr">
+          <td className="widgetLgUser">
+            <img
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="person"
+              className="widgetLgImg"
+            />
+            <span className="widgetLgName">Susan Carol</span>
+          </td>
+          <td className="widgetLgDate">12 Jan 2022</td>
+          <td className="widgetLgAmount">$122.00</td>
+          <td className="widgetLgStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default WidgetLg;
