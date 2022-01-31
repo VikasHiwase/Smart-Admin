ad67d96c75b72eae587e62bb6e1509f883a743b9
vikas hiwase <vikas.hiwase20@gmail.com>
2022-01-31 13:00:09 +0530

    pages added

---
+++ src/pages/home/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import { userData } from "../../DummyData";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        title="User Analytics"
+        data={userData}
+        dataKey="Active User"
+        grid
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
