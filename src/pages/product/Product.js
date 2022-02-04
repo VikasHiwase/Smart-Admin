f55a742c74f295aa5554f4e68a3075e36761b9be
vikas hiwase <vikas.hiwase20@gmail.com>
2022-02-04 14:27:46 +0530

    product page added

---
+++ src/pages/product/Product.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Chart from "../../components/chart/Chart";
+import "./product.css";
+import { productData } from "../../DummyData";
+import { Publish } from "@material-ui/icons";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">Product</h1>
+        <Link to="/newProduct">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+        <div className="productTopLeft">
+          <Chart data={productData} dataKey="Sales" title="Sale Performance" />
+        </div>
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <img
+              src="https://images.unsplash.com/photo-1585565804112-f201f68c48b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+              alt="product"
+              className="productInfoImg"
+            />
+            <span className="productName">Apple Airpods</span>
+          </div>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id:</span>
+              <span className="productInfoValue">123</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">sales:</span>
+              <span className="productInfoValue">5123</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">active:</span>
+              <span className="productInfoValue">yes</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">in stocks:</span>
+              <span className="productInfoValue">no</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>Product Name</label>
+            <input type="text" placeholder="Apple Airpod" />
+            <label>In Stock</label>
+            <select name="instock" id="idstock">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+            <label>Active</label>
+            <select name="active" id="active">
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img
+                src="https://images.unsplash.com/photo-1585565804112-f201f68c48b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                alt="upload"
+                className="productUploadImg"
+              />
+              <label for="file">
+                <Publish />
+              </label>
+              <input type="file" id="file" style={{ display: "none" }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
